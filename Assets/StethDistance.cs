736c298638a3cd2926dfde86c650e337f556323b
kwabbott <31709175+kwabbott@users.noreply.github.com>
2018-07-30 13:21:50 -0400

    Added FreezePhysics to StethDistance for Joint-Based

--- Assets/StethDistance.cs
+++ Assets/StethDistance.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
+using DG.Tweening;
+
 
 public class StethDistance : MonoBehaviour {
 
     public Transform stethEnd;
+    public Rigidbody stethEndRB;
     public float maxDistance;
     public float actualDistance;
 
@@ -15,25 +18,43 @@ public class StethDistance : MonoBehaviour {
 
 	public GameObject grabbedObject;
 	
+	public float jumpBackAmount;
+	
+	public Transform cubeA, cubeB;
+	
 	// Use this for initialization
 	void Start () {
-		
+		DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		//	cubeA.position = stethEnd.position + (gameObject.transform.position - (stethEnd.position/jumpBackAmount));
 
         actualDistance = Vector3.Distance(gameObject.transform.position, stethEnd.position);
 
-        
+     
+
         if (actualDistance > maxDistance)
         {
-	        //  Debug.Log(actualDistance);
-         //   stethEnd.position = stethEnd.position + (gameObject.transform.position - stethEnd.position)/2;
 
-	        if (grabber.GetGrabbedObject() == stethEnd.gameObject)
+              stethEndRB.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+             stethEndRB.constraints = RigidbodyConstraints.None;
+
+            //  Debug.Log(actualDistance);
+            //   stethEnd.position = stethEnd.position + (gameObject.transform.position - stethEnd.position)/2;
+
+            if (grabber.GetGrabbedObject() == stethEnd.gameObject)
 	        {
-		        grabber.ForceRelease(false);
+	            grabber.ForceRelease(true);
+	            
+	            Vector3 snapPos = stethEnd.position + (gameObject.transform.position - (stethEnd.position/jumpBackAmount));
+	            
+	            //  stethEnd.DOMove(cubeB.position, 0.5f, false);
+	            
+	            // stethEnd.DoMove(stethEnd.position + (gameObject.transform.position - (stethEnd.position)/jumpBackAmount), 0.5, false);
+	            //  stethEnd.position = stethEnd.position + (gameObject.transform.position - (stethEnd.position)/jumpBackAmount);
 	        }
 	        
 	      
